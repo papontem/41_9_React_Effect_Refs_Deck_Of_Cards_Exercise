97afe50d528e9938f6d92999d409f2f81e46970a
papontemjetix <papontem@gmail.com>
2023-12-11 19:35:04 -0500

    deckof cards component fleshing out

--- react_efx_ref_dek_cards/src/App.js
+++ react_efx_ref_dek_cards/src/App.js
@@ -8,9 +8,8 @@ function App() {
 	// Create a new React application, which contains the following components:
 
 	// App - this component should render the deck of cards, one card at a time.
-	// When the page loads, go to the Deck of Cards API to create a new deck, and show a button on the page that will let you draw a card.
-	//Every time you click the button, display a new card, until there are no cards left in the deck. If you try to draw when there are no cards remaining, an alert message should appear on the screen with the text “Error: no cards remaining!
-	// The goal here is to focus on React and not CSS so please get the core functionality working and then get a code review. Do not focus on CSS at all for now.
+	// DeckOfCards - Place your state that contains all of the cards here and use effects. This component should render the card component along with the button to draw a card component
+	console.log("APP MOUNTED");
 	return (
 		<div className="App">
 			<Header />
--- react_efx_ref_dek_cards/src/DeckOfCards.js
+++ react_efx_ref_dek_cards/src/DeckOfCards.js
@@ -1,8 +1,56 @@
+import axios from "axios";
+/*
+    - When the page loads, go to the Deck of Cards API to create a new deck, and show a button on the page that will let you draw a card.
+    - Every time you click the button, display a new card, until there are no cards left in the deck. If you try to draw when there are no cards remaining, an alert message should appear on the screen with the text “Error: no cards remaining!
+    - The goal here is to focus on React and not CSS so please get the core functionality working and then get a code review. Do not focus on CSS at all for now.
+*/
+
 function DeckOfCards(props) {
+    
+    console.log("DECKOFCARDS MOUNTED");
+    // useEffect to get deck of cards from the api and save it in state
+    // useState for both the deck and the card that currently selected.
+
+    const getDeck = () => {}
+    
+    
+    const drawCardHandler = () => {
+        console.log("YOU CLICKED DRAW A CARD!!");
+    }
+
     return(
         <div className="DeckOfCards">
-            <p>I AM A DECK OF CAAARRRRRDS</p>
+            <button className="DeckOfCards-draw-button" onClick={drawCardHandler} >Draw A Card</button>
+            <div className="DeckOfCards-card-container"> 
+                <img 
+                    className="DeckOfCards-card-img" 
+                    alt="I AM A CARD" 
+                    src="https://deckofcardsapi.com/static/img/back.png"
+                />
+                
+            </div>
         </div>
     )
 }
 export default DeckOfCards;
+
+
+// /** GitHub Profile Component --- shows info from GH API */
+
+// function ProfileViewer() {
+//   const [profile, setProfile] = useState(null);
+
+//   // this is called *after* component first added to DOM
+//   useEffect(function fetchUserWhenMounted() {
+//     async function fetchUser() {
+//       const userResult = await axios.get(
+//         "https://api.github.com/users/elie");
+//       setProfile(userResult.data);
+//     }
+//     fetchUser();
+//   }, []);
+
+//   return (
+//     <div>{profile ? <h2>{profile.name}</h2> : <i>(loading)</i>}</div>
+//   );
+// };
