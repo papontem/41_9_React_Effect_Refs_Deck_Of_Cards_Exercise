08be9eb0e5fc50421ed4f0ce85eb5ba314e847cd
papontemjetix <papontem@gmail.com>
2023-12-11 20:56:01 -0500

    part 1 of exercise complete, can now draw cards with api calls , see the deck id, the card that was drawn, and im storing the deck in local storage aswell as in state cus i wanted to save the DeckOfCards API dev some headache. component atcively checks if we have a deck or not when it gets mounted and when its rendered when state changes for the component.

--- react_efx_ref_dek_cards/src/DeckOfCards.js
+++ react_efx_ref_dek_cards/src/DeckOfCards.js
@@ -1,4 +1,7 @@
+// DeckOfCards.js
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+
 /*
     - When the page loads, go to the Deck of Cards API to create a new deck, and show a button on the page that will let you draw a card.
     - Every time you click the button, display a new card, until there are no cards left in the deck. If you try to draw when there are no cards remaining, an alert message should appear on the screen with the text “Error: no cards remaining!
@@ -6,51 +9,105 @@ import axios from "axios";
 */
 
 function DeckOfCards(props) {
-    
-    console.log("DECKOFCARDS MOUNTED");
-    // useEffect to get deck of cards from the api and save it in state
-    // useState for both the deck and the card that currently selected.
 
-    const getDeck = () => {}
+    console.log("DECKOFCARDS MOUNTED");
+    
+    // useState for both the deck and the card that will be used by this component
+    const [deck, setDeck] = useState('');
+    const [drawnCard, setDrawnCard] = useState('');
     
     
-    const drawCardHandler = () => {
+    // useEffect to get deck of cards from the api and save it in state
+    // this is called *after* component first added to DOM
+   useEffect(function fetchDeckOfCardsWhenMounted() {
+        // GET A NEW SHUFFLED DECK API CALL TO https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1
+        /** Example response
+            {
+                "success": true,
+                "deck_id": "3p40paa87x90",
+                "shuffled": true,
+                "remaining": 52
+            }
+        */
+        // Check if there's a deck in localStorage
+        const storedDeck = localStorage.getItem("deck");
+        if (storedDeck.remaining > 0) {
+            // If there's a deck in localStorage, use it while it has cards remaining
+            console.log("WE HAVE A DECK");
+            setDeck(JSON.parse(storedDeck));
+        } else {
+            // If there's no deck in localStorage, fetch a new one
+            console.log("GETTING A DECK");
+            // defining our async axios call
+            async function fetchDeck() {
+                try {
+                    const deckResult = await axios.get(
+                    "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+                    );
+                    setDeck(deckResult.data);
+                    // Store the deck in localStorage
+                    localStorage.setItem("deck", JSON.stringify(deckResult.data));
+                } catch (error) {
+                    console.error("Error fetching deck:", error);
+                }
+            }
+            // making the axios call to fecth the deck
+            fetchDeck();
+        }
+        
+    }, []); //Empty array to make it only execute once.
+    
+
+    const drawCardHandler = async () => {
+
         console.log("YOU CLICKED DRAW A CARD!!");
+        if (deck) {
+            try {
+            // pop a card from the top of the shuffled deck - use with api calls
+            // DRAW A CARD WITH THE API CALL TO https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=1
+            const cardDrawResult = await axios.get(
+                `https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`
+            );
+            console.log("Card Draw Result:", cardDrawResult);
+            console.log("Card Draw Result Data:", cardDrawResult.data);
+            // IF WE GET A NO MORE CARDS ERROR, LET THE USER KNOW with an alert message should appear on the screen with the text "Error: no cards remaining!" and delete the deck from local storage
+            if (cardDrawResult.data.remaining === 0) {
+                alert("Error: no cards remaining!");
+              } else {
+                // Update the remaining count in the deck
+                const updatedDeck = { ...deck, remaining: cardDrawResult.data.remaining };
+                setDeck(updatedDeck);
+                
+                // Update the deck in local storage
+                localStorage.setItem("deck", JSON.stringify(updatedDeck));
+        
+                setDrawnCard(cardDrawResult.data.cards[0]);
+              }
+            } catch (error) {
+              console.error("Error drawing card:", error);
+            }
+          }
     }
+
+      
 
     return(
         <div className="DeckOfCards">
+            <div className="DeckOfCards-info">
+                <p>deck id: <b>{deck ? <i>{deck.deck_id}</i> : <i>- loading -</i>}</b> </p>
+            </div>
             <button className="DeckOfCards-draw-button" onClick={drawCardHandler} >Draw A Card</button>
+
             <div className="DeckOfCards-card-container"> 
                 <img 
                     className="DeckOfCards-card-img" 
                     alt="I AM A CARD" 
-                    src="https://deckofcardsapi.com/static/img/back.png"
+                    src={drawnCard !== '' ? drawnCard.image : "https://deckofcardsapi.com/static/img/back.png" }
                 />
                 
             </div>
         </div>
     )
 }
+
 export default DeckOfCards;
-
-
-// /** GitHub Profile Component --- shows info from GH API */
-
-// function ProfileViewer() {
-//   const [profile, setProfile] = useState(null);
-
-//   // this is called *after* component first added to DOM
-//   useEffect(function fetchUserWhenMounted() {
-//     async function fetchUser() {
-//       const userResult = await axios.get(
-//         "https://api.github.com/users/elie");
-//       setProfile(userResult.data);
-//     }
-//     fetchUser();
-//   }, []);
-
-//   return (
-//     <div>{profile ? <h2>{profile.name}</h2> : <i>(loading)</i>}</div>
-//   );
-// };
